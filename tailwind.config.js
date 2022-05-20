d33674c802d78278ebe2989f7dcfd9b3aabf6fee
Sirmawan <you@example.com>
2022-05-20 13:53:18 +0700

    portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#6ee7b7",
+        dark: "#1f2937",
+        secondary: "#334155",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
